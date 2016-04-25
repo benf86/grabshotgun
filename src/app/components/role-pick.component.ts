949cae014fe8617409718332f80dfd456d93d9a0
Rolando Benjamin Vaz Ferreira <contact@ferreira.si>
2016-04-26 00:42:57 +0200

    add what had been done before

---
+++ src/app/components/role-pick.component.ts
@@ -0,0 +1,10 @@
+import { Component } from 'angular2/core';
+
+
+@Component({
+	selector: 'role-pick',
+	templateUrl: '../app/templates/html/role-pick.component.html'
+})
+export class RolePick {
+
+}
---
+++ src/app/controllers/routes/default.route.ts
@@ -0,0 +1,11 @@
+import { RolePick } from '../../components/role-pick.component';
+
+
+export var routes = [
+	{
+		path: '/',
+		name: 'RolePick',
+		component: RolePick,
+		useAsDefault: true
+	}
+]
