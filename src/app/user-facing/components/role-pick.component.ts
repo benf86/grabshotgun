31ab2e5d8dd7a0f10597294933cd694ed46fdbfe
Rolando Benjamin Vaz Ferreira <contact@ferreira.si>
2016-04-28 22:15:28 +0200

    add basic transition-animations to role cards

--- src/app/user-facing/components/role-pick.component.ts
+++ src/app/user-facing/components/role-pick.component.ts
@@ -1,4 +1,5 @@
 import { Component } from 'angular2/core';
+import { NgClass } from 'angular2/common';
 
 
 @Component({
@@ -6,5 +7,15 @@ import { Component } from 'angular2/core';
 	templateUrl: '../app/user-facing/templates/html/role-pick.component.html'
 })
 export class RolePick {
+	driver = {
+		cardState: 'maximized'
+	};
+	shotgun = {
+		cardState: 'maximized'
+	};
 
+	toggleMaximized (cardName: string) {
+		console.log(this[cardName].cardState);
+		this[cardName].cardState = this[cardName].cardState === 'maximized' ? 'minimized' : 'maximized';
+	}
 }
